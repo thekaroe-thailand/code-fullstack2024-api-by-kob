e3e899fd39ef083c7886aa32eae3771d4a072563
Tavon Seesenpila <thekaroe@gmail.com>
2024-05-19 14:56:32 +0700

    day14

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,8 +7,39 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+function checkSingIn(req, res, next) {
+    try {
+        const secret = process.env.TOKEN_SECRET;
+        const token = req.headers['authorization'];
+        const result = jwt.verify(token, secret);
+
+        if (result != undefined) {
+            next();
+        }
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+}
+function getUserId(req, res) {
+    try {
+        const secret = process.env.TOKEN_SECRET;
+        const token = req.headers['authorization'];
+        const result = jwt.verify(token, secret);
+
+        if (result != undefined) {
+            return result.id;
+        }
+    } catch (e) {
+        res.status(500).send({error: e.message});
+    }
+}
+
 app.post('/signIn', async (req, res) => {
     try {
+        if (req.body.user == undefined || req.body.pass == undefined) {
+            return res.status(401).send('unauthorized');
+        }
+
         const user = await prisma.user.findFirst({
             select: {
                 id: true,
@@ -20,7 +51,7 @@ app.post('/signIn', async (req, res) => {
                 status: 'use'
             }
         })
-        //console.log(user);
+
         if (user != null) {
             const secret = process.env.TOKEN_SECRET;
             const token = jwt.sign(user, secret, { expiresIn: '30d' });
@@ -31,6 +62,23 @@ app.post('/signIn', async (req, res) => {
         res.status(401).send({ message: 'unauthorized' });
     } catch (e) {
         res.status(500).send({ error: e.messsag });
+    }
+})
+app.get('/info', checkSingIn, async (req, res, next) => {
+    try {
+        const userId = getUserId(req, res);
+        const user = await prisma.user.findFirst({
+            select: {
+                name: true
+            },
+            where: {
+                id: userId
+            }
+        })
+
+        res.send({ result: user});
+    } catch (e) {
+        res.status(500).send({error: e.message});
     }
 })
 
