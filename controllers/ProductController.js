d8d96155ae479e74328b01b406ccf0e739f604f5
Tavon Seesenpila <thekaroe@gmail.com>
2024-05-20 22:48:11 +0700

    day16

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -18,5 +18,51 @@ app.post("/create", async (req, res) => {
         res.status(500).send({ error: e.message });
     }
 });
+app.get('/list', async (req, res) => {
+    try {
+        const data = await prisma.product.findMany({
+            orderBy: {
+                id: 'desc'
+            },
+            where: {
+                status: 'use'
+            }
+        })
+
+        res.send({ results: data });
+    } catch (e) {
+        res.status(500).send({ error: e.message });
+    }
+})
+app.delete('/remove/:id', async (req, res) => {
+    try {
+        await prisma.product.update({
+            data: {
+                status: 'delete'
+            },
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+
+        res.send({ message: 'success' })
+    } catch (e) {
+        res.status(500).send({ error: e.message });
+    }
+})
+app.put('/update', async (req, res) => {
+    try {
+        await prisma.product.update({
+            data: req.body,
+            where: {
+                id: parseInt(req.body.id)
+            }
+        });
+
+        res.send({ message: 'success' });
+    } catch (e) {
+        res.status(500).send({ error: e.message })
+    }
+})
 
 module.exports = app;
