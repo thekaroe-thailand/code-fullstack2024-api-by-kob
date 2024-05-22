e390b96a369b1244a104bb8e1c9ddba096cc2b07
Tavon Seesenpila <thekaroe@gmail.com>
2024-05-22 13:14:18 +0700

    day17

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -4,9 +4,11 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const fileUpload = require('express-fileupload');
 
 dotenv.config();
 
+app.use(fileUpload());
 app.post("/create", async (req, res) => {
     try {
         await prisma.product.create({
@@ -61,7 +63,40 @@ app.put('/update', async (req, res) => {
 
         res.send({ message: 'success' });
     } catch (e) {
-        res.status(500).send({ error: e.message })
+        res.status(500).send({ error: e.message });
+    }
+})
+app.post('/upload', async (req, res) => {
+    try {
+        if (req.files != undefined) {
+            if (req.files.img != undefined) {
+                const img = req.files.img;
+                const fs = require('fs');
+                const myDate = new Date();
+                const y = myDate.getFullYear();
+                const m = myDate.getMonth() + 1;
+                const d = myDate.getDate();
+                const h = myDate.getHours();
+                const mi = myDate.getMinutes();
+                const s = myDate.getSeconds();
+                const ms = myDate.getMilliseconds();
+
+                const arrFileName = img.name.split('.');
+                const ext = arrFileName[arrFileName.length - 1];
+
+                const newName = `${y}${m}${d}${h}${mi}${s}${ms}.${ext}`;
+
+                img.mv('./uploads/' + newName, (err) => {
+                    if (err) throw err;
+
+                    res.send({ newName: newName });
+                })
+            }
+        } else {
+            res.status(501).send('notimplemented');
+        }
+    } catch (e) {
+        res.status(500).send({ error: e.message });
     }
 })
 
